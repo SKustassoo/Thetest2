7fb10eae358c63825fa5184f2728df06db7032a4
Siim Kustassoo <skustass@itcollege.ee>
2018-10-17 16:19:51 +0300

    Update FirstOne.py

---
+++ FirstOne.py
@@ -0,0 +1,20 @@
+number1 = 4
+number2 = 5
+sone1 = "10"
+sone2 = "3"
+summa = number1+number2
+print("Summa on: ",summa)
+print(int(sone1)+int(sone2))
+print(str(number1)+str(number2))
+
+
+
+
+int1 = int(input("Sisesta oma pikkus: "))
+int2 = int(input("Sisesta oma vanus: "))
+
+identifikaator = int1*int2
+
+
+print("Teie identifikaator on on: ",int1*int2)
+print("Teie identifikaator on on: ",identifikaator)
